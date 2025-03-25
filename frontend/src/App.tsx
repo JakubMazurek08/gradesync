b3dc345b7d414fc3b8b9855da6398ac1990ec669
Jakub Mazurek <jakubmazurek08@gmail.com>
2025-03-25 15:18:02 +0100

    configured frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import {Router} from "./Router.tsx";
+
+function App() {
+
+  return (
+    <>
+        <Router/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return(
+        <h1>sigma</h1>
+    )
+}
