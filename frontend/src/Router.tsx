2e72368954aa58e1acee302d44eadd702567d08c
Maja Veratti <maj4eczka03@gmail.com>
2025-04-01 11:41:21 +0200

    added component footer

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,16 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import {Home} from "./pages/Home.tsx"
+import {Footer} from "./components/Footer.tsx";
 
 const router = createBrowserRouter([
     {
     path: '/',
     element: <Home/>
     },
+    {
+        path: '/footer',
+        element: <Footer />
+    }
 ])
 
 export const Router = () => {
