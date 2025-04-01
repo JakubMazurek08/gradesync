97e0c2092debf88f8c2bf72bab34e9b59d8af312
Kuba L. <u43_jakles_lbn@technischools.com>
2025-04-01 11:12:39 +0200

    dodanie lp do routera (i easter egg)

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,21 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import {Home} from "./pages/Home.tsx"
+import {EasterEgg} from "./pages/EasterEgg.tsx"
+import {LandingPage} from "./pages/LandingPage.tsx"
 
 const router = createBrowserRouter([
     {
     path: '/',
     element: <Home/>
     },
+    {
+        path: '/EasterEgg',
+        element: <EasterEgg/>
+    },
+    {
+        path: '/LandingPage',
+        element: <LandingPage/>
+    }
 ])
 
 export const Router = () => {
