3e0d2b0ef17b53bb3a4c3647e84848e502207e56
MichalK252 <u39_mickar_lbn@technischools.com>
2025-04-02 20:54:48 +0200

    Footer has been changed

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,14 +3,15 @@ import {Text} from "./Text.tsx";
 export const Footer = () => {
     return (
         <footer className="bg-mediumgray text-white p-16">
-            <div className="container mx-auto px-4 grid grid-cols-4 gap-4 min-h-[300px]">
-                {/* Logo */}
-                <div className="flex justify-start">
+            <div className="container mx-auto px-4 grid grid-cols-4 gap-32 min-h-[300px]">
+                {/* Logo - adjusted to expand leftward */}
+                <div className="flex justify-start items-start -ml-38">
                     <img src="/GradeSyncLogoDarkSmall.png" alt="logo" className="h-24" />
                 </div>
+
                 <div>
                     <Text type='h3'>About</Text>
-                    <ul className="mt-2 space-y-3">
+                    <ul className="mt-4 space-y-3">
                         <li className="py-2 text-lg text-lightgray">System</li>
                         <li className="py-2 text-lg text-lightgray">Features</li>
                         <li className="py-2 text-lg text-lightgray">Connect with Us</li>
@@ -19,24 +20,22 @@ export const Footer = () => {
                         <li className="py-2 text-lg text-lightgray">Trust Center</li>
                     </ul>
                 </div>
-                <div className="grid grid-cols-2 gap-8">
-                    <div>
-                        <Text type='h3'>Headquarters</Text>
-                        <ul className="mt-2 space-y-3">
-                            <li className="py-2 text-lg text-lightgray">1614 Santa Monica Blvd, Santa Monica, CA 90404</li>
-                            <li className="py-2 text-lg text-lightgray">+1 (312) 555-8392</li>
-                            <li className="py-2 text-lg text-lightgray">+1 (415) 555-7248</li>
-                            <li className="py-2 text-lg text-lightgray">Contact Us</li>
-                        </ul>
-                    </div>
+
+                <div>
+                    <Text type='h3'>Headquarters</Text>
+                    <ul className="mt-4 space-y-3">
+                        <li className="py-2 text-lg text-lightgray">1614 Santa Monica Blvd, Santa Monica, CA 90404</li>
+                        <li className="py-2 text-lg text-lightgray">+1 (312) 555-8392</li>
+                        <li className="py-2 text-lg text-lightgray">+1 (415) 555-7248</li>
+                        <li className="py-2 text-lg text-lightgray">Contact Us</li>
+                    </ul>
                 </div>
-                <div className="grid grid-cols-2 gap-8">
-                    <div>
-                        <Text type='h3'>Pricing</Text>
-                        <ul>
-                            <li className="py-4 text-lg text-lightgray">Pricing site</li>
-                        </ul>
-                    </div>
+
+                <div>
+                    <Text type='h3'>Pricing</Text>
+                    <ul className="mt-4 space-y-3">
+                        <li className="py-2 text-lg text-lightgray">Pricing site</li>
+                    </ul>
                 </div>
             </div>
         </footer>
