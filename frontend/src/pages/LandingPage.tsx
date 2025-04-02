e5814d1cde558b8fcb479e608fae0cedcb9b34f3
Kuba L. <u43_jakles_lbn@technischools.com>
2025-04-03 00:01:27 +0200

    added text to te landing page. initial look is done

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -9,14 +9,14 @@ export const LandingPage = () => {
         <NavBar />
       </div>
       <div className="relative z-20 h-[100vh] w-screen flex flex-row">
-        <div className="w-[50vw] flex flex-row">
+        <div className="w-[50vw] flex flex-row justify-center items-center pr-50 pb-60">
           <Text type="h1">
             <span className="flex flex-wrap items-center gap-2">
               GradeSync -
               <span className="ml-2">
                 <Typewriter
                   options={{
-                    strings: ["TRACK", "SYNC", "SUCCEED"],
+                    strings: ["TRACK", "SYNC", "SUCCEED!"],
                     autoStart: true,
                     loop: true,
                   }}
@@ -24,6 +24,17 @@ export const LandingPage = () => {
               </span>
             </span>
           </Text>
+        </div>
+        <div className="w-[50vw] flex flex-col justify-center items-center pt-40 gap-4">
+          <div className="w-[27vw] flex flex-wrap">
+            <Text>
+              GradesSync is a smart and intuitive platform for tracking grades,
+              upcoming assignments, and academic progress.
+            </Text>
+          </div>
+          <div className="flex flex-row justify-center pl-25">
+                  <button className="bg-white p-5 rounded-2xl font-semibold">GET STARTED ➜</button>
+          </div>
         </div>
       </div>
       <div className="bg-black opacity-70 absolute inset-0 z-0"></div>
