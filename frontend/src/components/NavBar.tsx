750f67a2d765996eb614754f8424adceb7dbe531
Kuba L. <u43_jakles_lbn@technischools.com>
2025-04-03 22:45:04 +0200

    added responsiveness

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,10 +2,9 @@ import { Text } from "./Text"
 
 export const NavBar = () => {
     return (
-        <div className="bg-gray-950 h-20 border-b-2 border-white flex flex-row justify-between items-center px-5">
-            <img src="/GradeSyncLogoDarkSmall.png" alt="LOGO" className="h-14"/>
+        <div className="bg-gray-950 h-20 max-w-[100vw] border-b-2 border-white flex flex-row justify-between items-center px-5">
+            <img src="/GradeSyncLogoDarkSmall.png" alt="LOGO" className="h-10"/>
             <div className="w-150 flex flex-row justify-around">
-                <Text type="h4">ENG</Text>
                 <Text type="h4">PRICING</Text>
                 <Text type="h4">LOGIN/SIGN UP</Text>
             </div>
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -4,40 +4,90 @@ import { NavBar } from "../components/NavBar";
 
 export const LandingPage = () => {
   return (
-    <div className="bg-[url(/LandingPageBgImage.png)] bg-center h-screen w-screen bg-[length:115%] flex flex-col">
-      <div className="relative z-10">
-        <NavBar />
-      </div>
-      <div className="relative z-20 h-[100vh] w-screen flex flex-row">
-        <div className="w-[50vw] flex flex-row justify-center items-center pr-50 pb-60">
-          <Text type="h1">
-            <span className="flex flex-wrap items-center gap-2">
-              GradeSync -
-              <span className="ml-2">
-                <Typewriter
-                  options={{
-                    strings: ["TRACK", "SYNC", "SUCCEED!"],
-                    autoStart: true,
-                    loop: true,
-                  }}
-                />
-              </span>
-            </span>
-          </Text>
+    <div className="max-w-[100vw] overflow-hidden">
+      <div className="bg-[url(/LandingPageBgImage.png)] bg-center h-screen max-w-[100vw] bg-cover lg:bg-[length:115%] flex flex-col relative">
+        <div className="bg-black opacity-70 absolute inset-0 z-0 h-screen"></div>
+
+        <div className="hidden lg:block relative z-10 max-w-[100vw]">
+          <NavBar />
         </div>
-        <div className="w-[50vw] flex flex-col justify-center items-center pt-40 gap-4">
-          <div className="w-[27vw] flex flex-wrap">
-            <Text>
-              GradesSync is a smart and intuitive platform for tracking grades,
-              upcoming assignments, and academic progress.
-            </Text>
+
+        <div className="relative z-10 h-full w-full flex flex-col md:flex-col lg:flex-row justify-center px-6 md:px-8">
+          <div className="w-full md:w-full lg:w-1/2 flex justify-center items-center mb-8 md:mb-8 lg:pb-50">
+            <div className="md:hidden text-center">
+              <Text type="h4">
+                <span className="flex flex-col items-center">
+                  GradeSync -
+                  <span className="mt-2">
+                    <Typewriter
+                      options={{
+                        strings: ["TRACK", "SYNC", "SUCCEED!"],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
+                  </span>
+                </span>
+              </Text>
+            </div>
+
+            <div className="hidden md:block lg:hidden text-center">
+              <Text type="h3">
+                <span className="flex flex-row items-center justify-center">
+                  GradeSync -
+                  <span className="ml-2">
+                    <Typewriter
+                      options={{
+                        strings: ["TRACK", "SYNC", "SUCCEED!"],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
+                  </span>
+                </span>
+              </Text>
+            </div>
+
+            <div className="hidden lg:block">
+              <Text type="h1">
+                <span className="flex flex-row items-center">
+                  GradeSync -
+                  <span className="ml-2">
+                    <Typewriter
+                      options={{
+                        strings: ["TRACK", "SYNC", "SUCCEED!"],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
+                  </span>
+                </span>
+              </Text>
+            </div>
           </div>
-          <div className="flex flex-row justify-center pl-25">
-                  <button className="bg-white p-5 rounded-2xl font-semibold">GET STARTED ➜</button>
+
+          <div className="w-full md:w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-center lg:pt-40">
+            <div className="w-full md:max-w-lg lg:w-[27vw] text-center lg:text-left">
+              <Text>
+                GradesSync is a smart and intuitive platform for tracking
+                grades, upcoming assignments, and academic progress.
+              </Text>
+            </div>
+            <div className="flex justify-center lg:justify-start mt-6 lg:pl-50 w-full md:w-auto lg:w-[27vw]">
+              {/* To gowno trza bedzie zlinkowac z logowaniem tylko nie mam tutaj komponentu */}
+              <button className="bg-white p-3 md:p-4 lg:p-5 rounded-xl md:rounded-xl lg:rounded-2xl font-semibold">
+                GET STARTED ➜
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-black opacity-70 absolute inset-0 z-0"></div>
+
+      <div className="bg-green-400 h-[100vh] w-[100vw] flex items-center justify-center">
+        <div className="text-center px-4">
+          <Text>Sigma</Text>
+        </div>
+      </div>
     </div>
   );
 };
