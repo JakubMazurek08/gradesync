654c6f287b3ea21f5d14f4189542f2ee4604fc3a
Jakub Mazurek <jakubmazurek08@gmail.com>
2025-04-10 22:37:22 +0200

    calendar finished, assignments are being displayed

--- backend/src/resources/user/user.controller.ts
+++ backend/src/resources/user/user.controller.ts
@@ -1,22 +1,23 @@
 import express, { Request, Response } from "express";
 import { dbClient } from "../../config/database";
+import {authenticationMiddleware} from "../../middleware/authentication.middleware";
 
 export const userController = express.Router();
 
-userController.get("/firstName/:id", async (req: Request, res: Response) => {
+userController.get("/firstName", authenticationMiddleware, async (req: Request, res: Response) => {
     if (!req.userId) {
         res.status(401).send({ error: "unauthorized" });
+        return;
     }
-
-    const requestedId = req.params.id;
 
     try {
         const result = await dbClient.query(
             `SELECT first_name FROM users WHERE id = $1`,
-            [requestedId]
+            [req.userId]
         );
 
         const firstName = result.rows[0]?.first_name;
+
 
         if (!firstName) {
             res.status(400).send("First name not found");
--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,15 +1,28 @@
 import {Text} from "../components/ui/Text.tsx";
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid';
 import {useEffect, useState} from "react";
 
-export const CalendarPage = () => {
-    const [timetable, setTimetable] = useState([]);
+type Course = {
+    startTime: string,
+    endTime: string,
+    courseName: string
+}
 
-    //REMOVE THIS LATER
-    console.log(timetable);
-    //REMOVE THIS LATER
+type Assignment = {
+    startTime: string,
+    endTime: string,
+    name: string,
+    category: string,
+    courseName: string,
+    date: string,
+}
+
+
+
+export const CalendarPage = () => {
+    const [events, setEvents] = useState<Course[]|Assignment[]>([]);
+
 
     useEffect(() => {
         const URL = import.meta.env.VITE_URL + "school/timetable/1";
@@ -18,7 +31,27 @@ export const CalendarPage = () => {
             method: 'GET',
             credentials: 'include',
         }).then(res => res.json().then((data)=>{
-            setTimetable(data);
+            console.log(data)
+
+            const newEvent = data.timetable.flatMap((dayOfTheWeek:Course[], index:number) => {
+                return dayOfTheWeek.map(course => ({
+                    title: course.courseName,
+                    daysOfWeek: [index+1],
+                    startTime: course.startTime,
+                    endTime: course.endTime,
+                    display: 'auto',
+                }));
+            });
+
+            const newAssignmentsEvents = data.assignments.map((assignment: Assignment) => ({
+                title: `${assignment.name} - ${assignment.category}`,
+                start: `${assignment.date}T${assignment.startTime}`, // Combine date and start time
+                end: `${assignment.date}T${assignment.endTime}`,     // Combine date and end time
+                display: 'auto',
+                color: '#bd1616', // Red color for assignments
+            }));
+
+            setEvents([...newEvent, ...newAssignmentsEvents]);
         }))
     }, []);
 
@@ -27,19 +60,16 @@ export const CalendarPage = () => {
             Calendar
         </Text>
         <FullCalendar
-            plugins={[ dayGridPlugin,  timeGridPlugin,]}
+            plugins={[ timeGridPlugin,]}
             initialView="timeGridWeek"
             weekends={false}
-            events={[
-                { title: 'event 1', date: '2025-04-08' },
-                { title: 'event 2', date: '2025-04-09' }
-            ]}
-            headerToolbar={{
-                left: 'prev,next',
-                center: 'title',
-                right: 'timeGridWeek,dayGridMonth'
-            }}
+            events={events}
+            eventColor={'#3c1596'}
+            hiddenDays={[6,7]}
             height={750}
+            slotDuration={'00:20:00'}
+            slotMinTime={'07:00:00'}
+            slotMaxTime={'18:00:00'}
         />
     </main>
 }
