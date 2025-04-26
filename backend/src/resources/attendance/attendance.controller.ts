995def44844c2e983c467258b7440d7caa838073
MichalK252 <u39_mickar_lbn@technischools.com>
2025-04-26 13:11:52 +0200

    for attendance controller get for :date has been added

--- backend/src/resources/attendance/attendance.controller.ts
+++ backend/src/resources/attendance/attendance.controller.ts
@@ -76,3 +76,61 @@ attendanceController.get('/sum', authenticationMiddleware, async (request: Reque
         response.status(500).send({message: 'internal server error', error: error})
     }
 })
+
+
+attendanceController.get('/date/:date', authenticationMiddleware, async (request: Request, response: Response) => {
+    if (!request.userId) {
+        response.status(401).send({error: "unauthorized"});
+        return;
+    }
+
+    const date = request.params.date;
+    // date format YYYYY-MM-DD
+    if (!date) {
+        response.status(400).send({error: "date parameter required"});
+        return;
+    }
+    try {
+        const result = await dbClient.query(`
+            SELECT 
+            a.id AS "attendanceId",
+            a.date AS "date",
+            a.status AS "status",
+            c.course_name AS "courseName",
+            d.lesson_hour AS "lessonHour",
+            d.day_of_the_week AS "dayOfWeek",
+            tu.first_name AS "teacherFirstName",
+            tu.last_name AS "teacherLastName"
+        FROM students s
+        JOIN attendance a ON s.id = a.student_id
+        JOIN users u ON s.user_id = u.id
+        JOIN days d ON 
+        CASE EXTRACT(DOW FROM a.date)
+            WHEN 0 THEN 'sunday'
+            WHEN 1 THEN 'monday'
+            WHEN 2 THEN 'tuesday'
+            WHEN 3 THEN 'wednesday'
+            WHEN 4 THEN 'thursday'
+            WHEN 5 THEN 'friday'
+            WHEN 6 THEN 'saturday'
+        END = d.day_of_the_week
+        JOIN courses c ON d.course_id = c.id
+        JOIN teachers t ON c.teacher_id = t.id
+        JOIN users tu ON t.user_id = tu.id
+        WHERE s.user_id = $1
+        AND a.date = $2
+        ORDER BY d.lesson_hour ASC `,
+        [request.userId, date]
+        );
+
+        if (result.rows.length > 0) {
+            response.send(result.rows);
+        } else {
+            response.send(404).send({message: "No attendance records for that day"});
+        }
+
+    } catch (error) {
+        response.status(500).send({message: 'internal server error', error: error})
+    }
+});
+
