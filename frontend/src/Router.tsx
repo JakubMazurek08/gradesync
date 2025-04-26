c26956cdaf7803added62e54c0859e871484bc06
Kuba L. <u43_jakles_lbn@technischools.com>
2025-04-26 13:43:14 +0200

    probably the end of the landing page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,33 +1,11 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import {Footer} from "./components/Footer.tsx";
-import {Dashboard} from "./pages/Dashboard.tsx"
-import {Navbar} from "./pages/Navbar.tsx";
-import {GradesPage} from "./pages/GradesPage.tsx";
 import {PricingLP} from "./pages/PricingLP.tsx";
-import {EasterEgg} from "./pages/EasterEgg.tsx"
 import {LandingPage} from "./pages/LandingPage.tsx"
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Navbar/>,
-        children:[
-            {
-                path: '/',
-                element: <Dashboard/>
-            },
-            {
-                path: '/grades',
-                element: <GradesPage/>
-            }
-        ]
-    },
-    {
-        path: '/EasterEgg',
-        element: <EasterEgg/>
-    },
-    {
-        path: '/LandingPage',
         element: <LandingPage/>
     },
     {
